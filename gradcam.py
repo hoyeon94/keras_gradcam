da46b8db2e3f7b07ab4542d499e47080612597f1
hoyeon <hoyeon351@gmail.com>
2021-05-25 18:22:16 +0900

    add compute_heatmap

--- gradcam.py
+++ gradcam.py
@@ -16,3 +16,33 @@ class GradCAM:
 			if len(layer.output_shape) == 4:
 				return layer.name
 		raise ValueError("Could not find 4D layer. Cannot apply GradCAM.")
+
+	def compute_heatmap(self, image, eps=1e-8):
+		gradModel = Model(
+			inputs=[self.model.inputs],
+			outputs=[self.model.get_layer(self.layerName).output,
+				self.model.output])
+
+		with tf.GradientTape() as tape:
+			inputs = tf.cast(image, tf.float32)
+			(convOutputs, predictions) = gradModel(inputs)
+			loss = predictions[:, self.classIdx]
+		grads = tape.gradient(loss, convOutputs)
+
+		castConvOutputs = tf.cast(convOutputs > 0, "float32")
+		castGrads = tf.cast(grads > 0, "float32")
+		guidedGrads = castConvOutputs * castGrads * grads
+		convOutputs = convOutputs[0]
+		guidedGrads = guidedGrads[0]
+
+		weights = tf.reduce_mean(guidedGrads, axis=(0, 1))
+		cam = tf.reduce_sum(tf.multiply(weights, convOutputs), axis=-1)
+
+		(w, h) = (image.shape[2], image.shape[1])
+		heatmap = cv2.resize(cam.numpy(), (w, h))
+
+		numer = heatmap - np.min(heatmap)
+		denom = (heatmap.max() - heatmap.min()) + eps
+		heatmap = numer / denom
+		heatmap = (heatmap * 255).astype("uint8")
+		return heatmap
